c1176644eaaae1a4176f1d47e4b948c4a0f61abd
Michael Burton <micbur1@gmail.com>
2021-02-06 17:46:16 -0800

    add first ever passing test! woo-hoogit add src/js/scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,8 @@
 let inputAge = 1
 let mercuryOutput = 0
 export default function mercuryAge(){
- 
+  const mercuryMultiplyer = .25
+  mercuryOutput = (inputAge / mercuryMultiplyer);
+  // console.log(mercuryOutput);
+  return mercuryOutput;
 };

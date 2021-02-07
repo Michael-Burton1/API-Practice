2e7aafce8723ddcf06f589df4665480798c44142
Michael Burton <micbur1@gmail.com>
2021-02-06 18:48:09 -0800

    add test for marsAge()

--- __tests__/test.scripts.js
+++ __tests__/test.scripts.js
@@ -1,20 +1,17 @@
 import { TestScheduler } from 'jest';
-import {mercuryAge, venusAge} from '../src/js/scripts.js';
+import {mercuryAge, venusAge, marsAge} from '../src/js/scripts.js';
 import mercuryOutput from '../src/js/scripts.js';
 import inputAge from '../src/js/scripts.js';
 
 
 
 describe('mercuryAge',() =>{
-  // let returnedArray= []
+  let returnedArray= []
   beforeEach(() =>{
     
     let inputAge = 1
-    // returnedArray=[10,9,8,7,6];
+    returnedArray=[10,9,8,7,6];
   });
-  // test('should return an age', () =>{
-  //   expect(mercuryAge(returnedArray)).toEqual([10,9,8,7,6])
-  // });
   test('should take inputAge and divide by mercuryMulitplier', () =>{
     expect(mercuryAge()).toEqual(4)
   });
@@ -22,10 +19,19 @@ describe('mercuryAge',() =>{
 
 describe('venusAge',() =>{
   beforeEach(() =>{
-    let inputAGE =1
+    let inputAge =1
   });
-  test('should take inputAge and divide by VenusMultiplyer', () =>{
+  test('should take inputAge and divide by VenusMultiplyer and round result to nearest tenth', () =>{
     expect(venusAge()).toEqual(1.6)
+  });
+});
+
+describe('marsAge',() =>{
+  beforeEach(() =>{
+    let inputAge = 1
+  });
+  test('should take inputAge and divide by marsMultiplyer. Then round result to nearest tenth',() =>{
+    expect (marsAge()).toEqual(.53)
   });
 });
 
@@ -37,8 +43,9 @@ describe('venusAge',() =>{
 
 
 
-
-
+// test('should return an age', () =>{
+//   expect(mercuryAge(returnedArray)).toEqual([10,9,8,7,6])
+// });
 
 // Person Class
 
